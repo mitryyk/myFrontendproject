7a5f982a2889388b87ef0fb7e90ccfac5bcda65d
mitryy <mitryy@yandex.ru>
2024-04-08 17:50:55 +0300

    commit 01

--- src/sharedpagecomponents/mainpagecardlink/index.tsx
+++ src/sharedpagecomponents/mainpagecardlink/index.tsx
@@ -5,8 +5,8 @@ export const CardLink = ({ text, pic }: { text: string; pic: string }) => {
   return (
     <div>
       <a href="#">
-        <div className="w3-card-4 w3-display-container" style={{ width: '100%' }}>
-          <img src={pic} alt={text} style={{ width: '100%', display: 'flex' }} className="w3-hover-opacity" />
+        <div className="w3-card-4 w3-display-container" style={{ maxWidth: 'auto', height: 'auto' }}>
+          <img src={pic} alt={text} style={{ width: '100%', maxHeight: '100%' }} className="w3-hover-opacity" />
           <div className="w3-display-bottomleft w3-container w3-padding-small w3-white w3-margin">{text}</div>
         </div>
       </a>
